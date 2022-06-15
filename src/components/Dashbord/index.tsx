d0e53a2fe2cb8f2672f06542c523b05ceda30243
orafapardim <orafapardim@gmail.com>
2022-06-15 16:29:17 -0300

    :construction: creating Dashbord component

---
+++ src/components/Dashbord/index.tsx
@@ -0,0 +1,10 @@
+import { Summary } from "../Summary";
+import { Containter } from "./styles";
+
+export function Dashbord() {
+  return (
+    <Containter>
+      <Summary />
+    </Containter>
+  )
+}
