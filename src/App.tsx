39357ec15000d95e23fd666137123231ab1b6714
orafapardim <orafapardim@gmail.com>
2022-06-15 16:38:51 -0300

    :building_construction: adding Dashbord component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Header } from "./components/Header";
+import { Dashbord } from "./components/Dashbord";
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashbord />
       <GlobalStyle />
     </>
   )
